de0bbc6734868874b702faef4777582fd3d4f540
Haruta T <hrtune@gmail.com>
2021-09-28 18:01:38 +0900

    Completed q2.c

--- homework/5/q2.c
+++ homework/5/q2.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[]){
     printf("Hello world! (pid: %d) \n", (int) getpid());
-    FILE *fp = fopen("hello.txt");
+    int filedesc = open("q2_text.txt", O_WRONLY | O_APPEND);
     int rc = fork();
     if(rc < 0){
         fprintf(stderr, "fork failed\n");
         exit(1);
     } else if (rc == 0){
         printf("Hello!, I am child (pid: %d)\n", (int) getpid());
+        if(filedesc < 0){
+            printf("The file can't be opened! (child)\n");
+            return 1;
+        }
+        write(filedesc, "I am child.\n", strlen("I am child.\n"));
+        
 
     } else {
         printf("Hello, I am parent of %d (pid: %d)\n", rc, (int) getpid());
-        printf("%s",)
+        if(filedesc < 0){
+            printf("The file can't be opened! (parent)\n");
+            return 1;
+        }
+        write(filedesc, "I am parent.\n", strlen("I am parent.\n"));
     }
     
-    fclose(fp);
+    close(filedesc);
 
     return 0;
 }
