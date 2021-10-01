a9d0a5747734810641ee39ad62ec891e6698de39
Haruta T <hrtune@gmail.com>
2021-10-01 19:42:48 +0900

    Create q4.c

---
+++ homework/5/q4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#define PATH "/bin/ls"
+
+int main(int argc, char *argv[]){
+    // create file "q3.txt" contains single character "F"
+    
+    if(argc < 2){
+        printf("error: no argument\n");
+        exit(1);
+    }
+
+    int sel = atoi(argv[0]);
+    
+    int rc = fork();
+    if(rc < 0){
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0){
+        char *args[] = {PATH, NULL};
+        switch(sel){
+            case 0:
+                execv(args[0], args);
+                break;
+            
+            case 1:
+                execl(PATH, NULL);
+                break;
+            
+        }
+    } else {
+        wait(NULL);
+        printf("goodbye!\n");
+    }
+    return 0;
+}
